7456ba6135720ee580f1a38ae95370a9cb891725
edersonmangueira <ederson.mangueira@hotmail.com>
2020-01-04 09:19:48 -0300

    Criando o projeto

---
+++ src/main/java/com/edersonfm/gestaodegastos/GestaoDeGastosApplication.java
@@ -0,0 +1,13 @@
+package com.edersonfm.gestaodegastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeGastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edersonfm/gestaodegastos/GestaoDeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edersonfm.gestaodegastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
